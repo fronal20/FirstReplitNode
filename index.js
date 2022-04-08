771b76076400f45a2d0d5ea0c4585610d5e6898c
Ronald fuentes <103284157+fronal20@users.noreply.github.com>
2022-04-08 20:31:32 +0000

    new clas product

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import {User} from './User.js'
+import {Product} from './Product.js'
 
 const ryan = new User('Ryan','Debian')
 console.log(ryan);
+
+const product = new Product('Azucar morena','20')
+console.log(product);
